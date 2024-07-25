a98a3fc106ebf11dcc686bf27c22537cf155ce1c
emesefedev <emesefedev@gmail.com>
2024-07-25 15:13:35 +0200

    Exercise 5 passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0
 
-};
+    const areIntegers = Number.isInteger(start) && Number.isInteger(end)
+    const arePositive = start > 0 && end > 0
+
+    if (!areIntegers || !arePositive) return "ERROR"
+
+    if (start > end)
+    {
+        const aux = start
+        start = end
+        end = aux
+    }
+
+
+    for (let i = start; i <= end; i++) {
+        sum += i
+    }
+    return sum
+}
 
 // Do not edit below this line
 module.exports = sumAll;
