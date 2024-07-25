c8a09c39f83c525a38ad3b9e895adc894623fa58
emesefedev <emesefedev@gmail.com>
2024-07-25 14:16:39 +0200

    Exercise 2 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, totalRepetitions) {
+    if (totalRepetitions >= 0) {
+        return stringToRepeat.repeat(totalRepetitions)
+    } 
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
