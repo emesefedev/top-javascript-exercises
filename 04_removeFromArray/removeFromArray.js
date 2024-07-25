503768df101e64475bad20f3662825cb16ed6884
emesefedev <emesefedev@gmail.com>
2024-07-25 14:40:38 +0200

    Exercise 4 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array) {
+
+    for (let i = 1; i < arguments.length; i++) {
+        const elementToRemove = arguments[i]
+        while(array.includes(elementToRemove)) {
+            const elementIdx = array.indexOf(elementToRemove)
+            array.splice(elementIdx, 1)
+
+        }
+        
+    }
+
+    return array
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
