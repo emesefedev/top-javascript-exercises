93c2acdc5875dea108ed3ec2fc0b039e675cdcce
emesefedev <emesefedev@gmail.com>
2024-07-25 15:25:03 +0200

    Exercise 7 passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
-};
+// celsius to fahrenheit: x * 9 / 5 + 32
+// fahrenheit to celsius: (x - 32) * 5 / 9
 
-const convertToFahrenheit = function() {
-};
+const convertToCelsius = function(fahrenheit) {
+  const celsius =  (fahrenheit - 32) * 5 / 9
+  return parseFloat(celsius.toFixed(1))
+}
+
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = celsius * 9 / 5 + 32
+  return parseFloat(fahrenheit.toFixed(1))
+}
 
 // Do not edit below this line
 module.exports = {
