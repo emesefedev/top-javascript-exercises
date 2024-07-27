b230d786d17b9ae59d4c23eee1c550591310e5b8
emesefedev <emesefedev@gmail.com>
2024-07-27 22:16:58 +0200

    Exercise 10 passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(item) {
+    item = +item
+    if (item < 0) return "OOPS"
+    if (item === 0) return 0
+    if (item === 1 || item === 2) return 1
+    return fibonacci(item - 1) + fibonacci(item - 2)
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
